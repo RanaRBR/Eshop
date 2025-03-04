b83caa98e2a3eaf847d8921f10a50b07e69cabd9
ivexxck <21thepyo13@gmail.com>
2025-03-04 02:34:09 -0800

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
+import Header from './components/header/Header'
 import './App.sass'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <Header />
     </>
   )
 }
